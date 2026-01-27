5ab58086a369f62b9aca92ec6b19213e9653ebfb
woochan0408 <woochan0408@naver.com>
2026-01-27 15:21:34 +0900

    chore: initialize Spring Boot project structure

---
+++ src/main/java/com/dailyonepage/backend/DailyOnePageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dailyonepage.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyOnePageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyOnePageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailyonepage/backend/DailyOnePageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyonepage.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyOnePageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
